9ba26321d37741c60394f586f1f5034f0ce946dd
Mike Kinney <mike.kinney@gmail.com>
2022-11-03 09:54:19 -0700

    use git to determine age of the file, then if it meets the criteria, purge the fiel

---
+++ purge_zips.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Use git to find and remove all .zip files that are older than 90 days
+date=$(date -v -90d +%Y-%m-%d)
+echo "date:${date}"
+git ls-files '*.zip' | while read path
+do
+  if [ "$(git log --since \"$date\" -- $path)" == "" ]; then
+    echo "Removing: $path"
+    rm "$path"
+  fi
+done

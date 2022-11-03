081a5d74e45b1fbbd0ff532b8d590b44d4e72401
Mike Kinney <mike.kinney@gmail.com>
2022-11-03 14:50:25 -0700

    tweak

--- purge_zips.sh
+++ purge_zips.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Use git to find and remove all .zip files that are older than 90 days
-date=$(date -d '90 days ago' +%Y-%m-%d)
-echo "date:${date}"
+#date=$(date -d '90 days ago' +%Y-%m-%d)
+#echo "date:${date}"
 git ls-files '*.zip' | while read path junk
 do
-  output=$(git log --since \"$date\" -- \"$path\")
+  output=$(git log --since='90 days ago' -- \"$path\")
   echo "path:${path}: output:${output}:"
   # If there is output, then the file has been created
   # within the window, so do not remove those.
